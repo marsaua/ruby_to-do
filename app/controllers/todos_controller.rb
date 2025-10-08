a4fa14acf6bba9fb4be498a3ad7cbe288069a022
marsaua001 <marsaua001@gmail.com>
2025-10-08 10:55:18 +0300

    feat: edit and delete methods for todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,15 @@
 class TodosController < ApplicationController
+    allow_unauthenticated_access only: %i[index show]
+    before_action :set_todo, only: %i[show edit update destroy]
+
     def index
       @todos = current_user.todos.order(created_at: :desc)
+    end
+
+    def show; end
+
+    def new
+        @todo = Todo.new
     end
   
     def create
@@ -11,15 +20,30 @@ class TodosController < ApplicationController
         redirect_to root_path, alert: "Failed to create todo: #{@todo.errors.full_messages.to_sentence}", status: :see_other
       end
     end
+
+    def edit; end
+
+    def update
+        if @todo.update(todo_params)
+            redirect_to root_path, notice: "Todo was successfully updated.", status: :see_other
+        else
+            redirect_to root_path, alert: "Failed to update todo: #{@todo.errors.full_messages.to_sentence}", status: :see_other
+        end
+    end
   
     def destroy
-      current_user.todos.find(params[:id]).destroy
+      @todo.destroy
       redirect_to root_path, notice: "Todo was successfully deleted.", status: :see_other
     end
   
     private
+    def set_todo
+        @todo = current_user.todos.find(params[:id])
+    end
     def todo_params
       params.require(:todo).permit(:title)
     end
+    
+    
   end
   
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
+    allow_unauthenticated_access only: :index # або skip_before_action :require_authentication
+  
     def index
-        if current_user
-            @todos = current_user.todos.order(created_at: :desc)
-        else
-            @todos = []
-        end
+      if current_user
+        @todos = current_user.todos.order(created_at: :desc)
+        @todo  = current_user.todos.build 
+      else
+        @todos = []
+        @todo  = Todo.new
+      end
     end
-end
+  end
